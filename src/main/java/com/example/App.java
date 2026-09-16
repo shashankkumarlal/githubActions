ff35d67caa21d78340e866baf11b432c99fdc818
Shashank Kumar Lal <shashanklal309@gmail.com>
2026-09-16 11:52:36 +0530

    Add Maven project and CI workflow

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
